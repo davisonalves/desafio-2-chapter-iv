58a6fc8998e3309a6a72ed41ab8d7f61ec86d72c
Davison Alves <53572219+davisonalves@users.noreply.github.com>
2021-10-19 15:35:44 -0400

    test: finalizando as asserções

--- cypress/integration/cadastro-de-clientes.spec.js
+++ cypress/integration/cadastro-de-clientes.spec.js
@@ -9,8 +9,9 @@ describe('Cadastro de clientes', () => {
         passwd: faker.internet.password()
     };
     it('Quando eu informar os dados do cliente e finalizar, então o cadastro deve ser efetuado', () => {
-        cy.visit('http://automationpractice.com/index.php?controller=authentication&back=my-account');
+        cy.visit('/index.php?controller=authentication&back=my-account');
 
+        /*Minhas asserções estavam dando erro algumas vezes por essa requisição demorar, então interceptei ela e fiz um wait para que antes das asserções o cypress espere essa requisição acabar*/
         cy.intercept({
             method: 'POST', 
             pathname: '/index.php'
@@ -26,7 +27,10 @@ describe('Cadastro de clientes', () => {
         cy.get('input#id_gender1').check();
         cy.get('input#customer_firstname').type(user.firstName);
         cy.get('input#customer_lastname').type(user.lastName);
+
+        /*O email que veio preenchido deve ser o mesmo que eu preenchi na tela anterior*/
         cy.get('input[name=email]').should('have.value',user.email);
+
         cy.get('input#passwd').type(user.passwd);
         cy.get('select#days').select('26');
         cy.get('select#months').select('February');
@@ -40,15 +44,15 @@ describe('Cadastro de clientes', () => {
         cy.get('input#address1').type(faker.address.streetAddress());
         cy.get('input#city').type(faker.address.city());
         cy.get('input#address2').type(faker.address.secondaryAddress());
-        cy.get('select#id_state').select(faker.address.state());
-        cy.get('input#postcode').type(faker.address.zipCode());
+        cy.get('input#postcode').type('01352'); //tentei colocar esse dado com Faker mas ele sempre gera um zipcode maior do que 5 dígitos e a página da erro ao finalizar cadastro alegando que é um zipcode fora do formato esperado, então mockei um zipcode
         cy.get('select#id_country').select('United States');
-        cy.get('input#phone_mobile').type(faker.phone.phoneNumber());
+        cy.get('select#id_state').select(faker.address.state());
+        cy.get('input#phone_mobile').type(faker.phone.phoneNumberFormat());
         cy.get('input#alias').type(faker.address.streetAddress());
         cy.get('button#submitAccount').click();
 
-        cy.url().should('contain', 'my-account')
+        /*Quando finalizo meu cadastro, deve aparecer um botão para conferir minhas informações*/
+        cy.get('a[title=Information]').should('be.visible');
 
-        /*http://automationpractice.com/index.php?controller=authentication*/
     });
 });
